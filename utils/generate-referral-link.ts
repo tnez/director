8d0384f874c1224210677a8b621196d93e11e7e1
Travis Nesland <tnesland@gmail.com>
2021-11-16 11:27:21 -0500

    Basic behavior for `/get-link` page
    Satisfies the following acceptance criteria:
    
    - referrer can visit `/get-link`, and...
      - enter their email address into the form
      - receive a unique referral link

---
+++ utils/generate-referral-link.ts
@@ -0,0 +1,20 @@
+import * as uuid from 'uuid'
+
+type GenerateReferralLinkInput = {
+  /**
+   * The root of the domain.
+   */
+  domain: string
+}
+
+/**
+ * Return a randomly generated referral link.
+ *
+ * @example
+ * generateReferralLink({ domain: 'sub.domain.io'})
+ * // => 'sub.domain.io/abcd-1234'
+ */
+export function generateReferralLink({ domain }: GenerateReferralLinkInput) {
+  const code = uuid.v4().split('-')[0]
+  return domain + '/' + code
+}
